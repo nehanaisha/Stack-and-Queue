8f1b05f8c6f50ca4070ce1f16f8c29ec3c8c90e0
nehanaisha <nehachaurasia682@gmail.com>
2022-06-15 00:00:01 +0530

    peekandpop

--- stack/StackAndQueue.java
+++ stack/StackAndQueue.java
@@ -5,6 +5,8 @@ import java.util.Stack;
 public class StackAndQueue {
 		public static void main(String[] args) {
 		StackAndQueue list = new StackAndQueue();
+		list.create();
+		list.peekAndPop();
 	}
 void create() {
 	Stack<Integer> st= new Stack<>();  
@@ -13,5 +15,14 @@ void create() {
     st.push(70);
     System.out.println("the element are: " + st);
 }
-	
+	void peekAndPop() {
+		Stack<Integer> st1= new Stack<>();  
+	    st1.push(56);
+	    st1.push(30);
+	    st1.push(70);
+	    System.out.println("the element are: " + st1);
+	    System.out.println("the element are: " + st1.peek());
+	    System.out.println("the element are: " + st1.pop());
+		
+	}
 }
