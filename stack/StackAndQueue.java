2ac69e85f589b0aa205e02a7131db19f48715151
nehanaisha <nehachaurasia682@gmail.com>
2022-06-15 00:08:29 +0530

    queue

--- stack/StackAndQueue.java
+++ stack/StackAndQueue.java
@@ -1,28 +1,43 @@
 package stack;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class StackAndQueue {
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 		StackAndQueue list = new StackAndQueue();
 		list.create();
 		list.peekAndPop();
+		list.queue();
 	}
-void create() {
-	Stack<Integer> st= new Stack<>();  
-    st.push(56);
-    st.push(30);
-    st.push(70);
-    System.out.println("the element are: " + st);
-}
+
+	void create() {
+		Stack<Integer> st = new Stack<>();
+		st.push(56);
+		st.push(30);
+		st.push(70);
+		System.out.println("the element are: " + st);
+	}
+
 	void peekAndPop() {
-		Stack<Integer> st1= new Stack<>();  
-	    st1.push(56);
-	    st1.push(30);
-	    st1.push(70);
-	    System.out.println("the element are: " + st1);
-	    System.out.println("the element are: " + st1.peek());
-	    System.out.println("the element are: " + st1.pop());
-		
+		Stack<Integer> st1 = new Stack<>();
+		st1.push(56);
+		st1.push(30);
+		st1.push(70);
+		System.out.println("the element are: " + st1);
+		System.out.println("the element are: " + st1.peek());
+		System.out.println("the element are: " + st1.pop());
+	}
+
+	void queue() {
+		Queue<Integer> que = new LinkedList<Integer>();
+		que.add(56);
+		que.add(30);
+		que.add(70);
+		System.out.println("the element are: " + que);
+		System.out.println("the element are: " + que.remove());
+		System.out.println("the element are: " + que).;
+
 	}
 }
