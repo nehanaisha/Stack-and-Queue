0be4b98d95833f9e40e63017529f0274ffcdabcd
nehanaisha <nehachaurasia682@gmail.com>
2022-06-14 19:51:14 +0530

    Stack crreation

---
+++ stack/StackAndQueue.java
@@ -0,0 +1,17 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackAndQueue {
+		public static void main(String[] args) {
+		StackAndQueue list = new StackAndQueue();
+	}
+void create() {
+	Stack<Integer> st= new Stack<>();  
+    st.push(56);
+    st.push(30);
+    st.push(70);
+    System.out.println("the element are: " + st);
+}
+	
+}
